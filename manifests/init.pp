a5aa0747605a918d82b9d73773836f845fb60f85
Jake Luer <jake.luer@gmail.com>
2012-05-26 15:32:43 -0400

    check if build-essential is defined as package

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,13 @@
-class redis($redis_ver = '2.4.13') {
+class redis($redis_ver = '2.4.14') {
 
   $redis_tar = "redis-$redis_ver.tar.gz"
   $redis_dl = "http://redis.googlecode.com/files/$redis_tar"
+
+  if defined(Package['build-essential']) == false {
+    package { "build-essential":
+      ensure => "installed"
+    }
+  }
 
   group { "redis":
     ensure => present
